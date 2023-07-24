91902c7a0d763888c25ec3c4db4ed64bf76d56dd
EuRV <eugenirovensky@yandex.ru>
2023-07-24 20:23:13 +0600

    feat: implement routing

--- server/plugin.js
+++ server/plugin.js
@@ -1,4 +1,34 @@
 // EARLYBIRDSSQL10
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fastifyStatic from '@fastify/static';
+import fastifyView from '@fastify/view';
+import Pug from 'pug';
+
+import addRoutes from './routes/index.js';
+
+const __dirname = fileURLToPath(path.dirname(import.meta.url));
+
+const setUpViews = (app) => {
+  app.register(fastifyView, {
+    engine: {
+      pug: Pug,
+    },
+    includeViewExtension: true,
+    defaultContext: {
+      assetPath: (filename) => `/assets/${filename}`,
+    },
+    templates: path.join(__dirname, '..', 'server', 'views'),
+  });
+};
+
+const setUpStaticAssets = (app) => {
+  const pathPublic = path.join(__dirname, '..', 'dist');
+  app.register(fastifyStatic, {
+    root: pathPublic,
+    prefix: '/assets/',
+  });
+};
 
 export const opts = {
   dotenv: true,
@@ -6,7 +36,7 @@ export const opts = {
 }
 
 export default async (fastify, opts) => {
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' };
-  })
+  setUpViews(fastify);
+  setUpStaticAssets(fastify);
+  addRoutes(fastify);
 };
