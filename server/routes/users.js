37f171a74182d353e6645602f8bad478ce27e5bf
EuRV <eugenirovensky@yandex.ru>
2023-07-28 15:10:45 +0600

    feat: implement a database query

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,21 @@
+import knex from '../../knex.js';
+
 export default (app) => {
   app
-    .get('/users', { name: 'users' }, (req, reply) => {
-      reply.view('users/index');
+    .get('/users', { name: 'users' }, async (req, reply) => {
+      try {
+        const users = await knex('users')
+          .select({
+            id: 'id',
+            userName: 'first_name',
+            email: 'email',
+            createdAt: 'created_at',
+          });
+        console.log(users);
+        reply.view('users/index', { users });
+        return reply;
+      } catch (err) {
+        console.error(err);
+      }
     });
 };
