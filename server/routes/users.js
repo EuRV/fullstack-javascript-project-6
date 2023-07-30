0119bf996e8f8801dac5895f6cf765e1e4729653
EuRV <eugenirovensky@yandex.ru>
2023-07-31 01:34:11 +0600

    feat: implement the get/user/new and post/users paths

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,21 +1,28 @@
-import knex from '../../knex.js';
-
 export default (app) => {
   app
     .get('/users', { name: 'users' }, async (req, reply) => {
       try {
-        const users = await knex('users')
-          .select({
-            id: 'id',
-            userName: 'first_name',
-            email: 'email',
-            createdAt: 'created_at',
-          });
-        console.log(users);
+        const users = await app.objection.models.user.query();
         reply.view('users/index', { users });
         return reply;
       } catch (err) {
         console.error(err);
+      }
+    })
+    .get('/users/new', { name: 'newUser' }, (req, reply) => {
+      const user = new app.objection.models.user();
+      reply.view('users/new', { user });
+    })
+    .post('/users', async (req, reply) => {
+      const user = new app.objection.models.user();
+      user.$set(req.body.data);
+
+      try {
+        const validUser = await app.objection.models.user.fromJson(req.body.data);
+        console.log(validUser);
+        await app.objection.models.user.query().insert(validUser);
+      } catch({ data }) {
+        console.error(data);
       }
     });
 };
