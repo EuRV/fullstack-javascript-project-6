1835c367f36360cd6f0426940500f556c4cb413b
EuRV <eugenirovensky@yandex.ru>
2023-07-31 01:12:35 +0600

    feat: implement the formation of queries to the database

--- server/plugin.js
+++ server/plugin.js
@@ -3,27 +3,32 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import fastifyStatic from '@fastify/static';
 import fastifyView from '@fastify/view';
+import fastifyFormbody from '@fastify/formbody';
+import { plugin as fastifyReverseRoutes } from 'fastify-reverse-routes';
+import fastifyObjectionjs from 'fastify-objectionjs';
+import qs from 'qs';
 import Pug from 'pug';
 import i18next from 'i18next';
 
 import ru from './locales/ru.js';
 import addRoutes from './routes/index.js';
+import getHelpers from './helpers/index.js';
+import * as knexConfig from '../knexfile.js';
+import models from './models/index.js';
 
+const mode = process.env.NODE_ENV || 'development';
 const __dirname = fileURLToPath(path.dirname(import.meta.url));
 
 const setUpViews = (app) => {
+  const helpers = getHelpers(app);
   app.register(fastifyView, {
     engine: {
       pug: Pug,
     },
     includeViewExtension: true,
     defaultContext: {
-      t: (key) => i18next.t(key),
+      ...helpers,
       assetPath: (filename) => `/assets/${filename}`,
-      formatDate: (str) => {
-        const date = new Date(str);
-        return date.toLocaleString();
-      },
     },
     templates: path.join(__dirname, '..', 'server', 'views'),
   });
@@ -47,12 +52,22 @@ const setupLocalization = async () => {
     });
 };
 
+const registerPlugins = async (app) => {
+  await app.register(fastifyReverseRoutes);
+  await app.register(fastifyFormbody, { parser: qs.parse });
+  await app.register(fastifyObjectionjs, {
+    knexConfig: knexConfig[mode],
+    models,
+  });
+};
+
 export const opts = {
   dotenv: true,
   data: process.env
 }
 
 export default async (fastify, opts) => {
+  await registerPlugins(fastify);
   await setupLocalization();
   setUpViews(fastify);
   setUpStaticAssets(fastify);
