09f2c6881e0a995d36e6dc603241beec36c5d1bf
EuRV <eugenirovensky@yandex.ru>
2023-07-31 01:20:10 +0600

    chore: implement the user model

---
+++ server/models/User.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+// import * as objectionUnique from 'objection-unique';
+// import BaseModel from './BaseModel';
+import encrypt from '../lib/secure.js';
+import objection from 'objection';
+const { Model } = objection;
+
+// const unique = objectionUnique({ fields: ['email'] });
+
+export default class User extends Model {
+  static get tableName() {
+    return 'users';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['email', 'password'],
+      properties: {
+        id: { type: 'integer' },
+        first_name: { type: 'string', minLength: 1 },
+        last_name: { type: 'string', minLength: 1 },
+        email: { type: 'string', minLength: 1 },
+        password: { type: 'string', minLength: 3 },
+      },
+    };
+  }
+
+  set password(value) {
+    this.passwordDigest = encrypt(value);
+  }
+
+  verifyPassword(password) {
+    return encrypt(password) === this.passwordDigest;
+  }
+}
