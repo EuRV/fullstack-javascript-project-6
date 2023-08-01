bfa192669106869b6ee41ec43ded529dfc135ad1
EuRV <eugenirovensky@yandex.ru>
2023-08-01 20:24:11 +0600

    chore: add internationalization variables

--- server/locales/ru.js
+++ server/locales/ru.js
@@ -41,9 +41,16 @@ export default {
         email: 'Email',
         createdAt: 'Дата создания',
         actions: 'Действия',
+        edit: 'Изменить',
+        editLabel: 'Изменение пользователя',
+        delete: 'Удалить',
         new: {
           submit: 'Сохранить',
           signUp: 'Регистрация',
+          email: 'email',
+          password: 'пароль',
+          firstName: 'Имя',
+          lastName: 'Фамилия',
         },
       },
       welcome: {
