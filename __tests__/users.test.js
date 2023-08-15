9c1654b1f5000c445c278b93d65c75e1657f9c6e
EuRV <eugenirovensky@yandex.ru>
2023-08-15 16:33:41 +0600

    test: implement a test of user routes

---
+++ __tests__/users.test.js
@@ -0,0 +1,124 @@
+import {
+  describe, beforeAll, beforeEach, it, expect, afterAll, afterEach,
+} from '@jest/globals';
+
+import _ from 'lodash';
+import fastify from 'fastify';
+
+import init from '../server/plugin.js';
+import encrypt from '../server/lib/secure.js';
+import { getTestData, prepareData, signIn } from './helpers/index.js';
+
+describe('test users CRUD', () => {
+  let app;
+  let knex;
+  let models;
+  let cookie;
+  let testData;
+
+  beforeAll(async () => {
+    app = fastify({
+      exposeHeadRoutes: false,
+      // logger: { target: 'pino-pretty' },
+    });
+    await init(app);
+    knex = app.objection.knex;
+    models = app.objection.models;
+  });
+
+  beforeEach(async () => {
+    await knex.migrate.latest();
+    await prepareData(app);
+    testData = getTestData();
+    cookie = await signIn(app, testData.users.existing);
+  });
+
+  it('index', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/users',
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('new', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/users/new',
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('create', async () => {
+    const params = testData.users.new;
+    const response = await app.inject({
+      method: 'POST',
+      url: '/users',
+      payload: {
+        data: params,
+      },
+    });
+
+    expect(response.statusCode).toBe(302);
+    const expected = {
+      ..._.omit(params, 'password'),
+      passwordDigest: encrypt(params.password),
+    };
+    const user = await models.user.query().findOne({ email: params.email });
+    expect(user).toMatchObject(expected);
+  });
+
+  it('edit', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/users/2/edit',
+      cookies: cookie,
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('update', async () => {
+    const params = testData.users.update;
+    const response = await app.inject({
+      method: 'PATCH',
+      url: '/users/2',
+      cookies: cookie,
+      payload: {
+        data: params,
+      },
+    });
+
+    expect(response.statusCode).toBe(302);
+    const expected = {
+      ..._.omit(params, 'password'),
+      passwordDigest: encrypt(params.password),
+    };
+    const user = await models.user.query().findOne({ email: params.email });
+    expect(user).toMatchObject(expected);
+  });
+
+  it('delete', async () => {
+    const params = testData.users.existing;
+    const response = await app.inject({
+      method: 'DELETE',
+      url: '/users/2',
+      cookies: cookie,
+    });
+
+    expect(response.statusCode).toBe(302);
+    const user = await models.user.query().findOne({ email: params.email });
+    expect(user).toBeUndefined();
+  });
+
+  afterEach(async () => {
+    // await knex.migrate.rollback();
+    await knex('users').truncate();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
