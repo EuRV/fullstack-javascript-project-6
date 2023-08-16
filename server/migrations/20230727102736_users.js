01069a4b70a4f24614a70492e526bccfb0488492
EuRV <eugenirovensky@yandex.ru>
2023-08-16 14:37:01 +0600

    fix: timestaps

--- server/migrations/20230727102736_users.js
+++ server/migrations/20230727102736_users.js
@@ -7,8 +7,7 @@ export const up = (knex) => (
     table.string('last_name');
     table.string('email').unique();
     table.string('password_digest');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.timestamps(true, true);
   })
 );
 
