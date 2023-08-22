b036f124a1e50d1107a2c2cdebe893596d8d83cb
EuRV <eugenirovensky@yandex.ru>
2023-08-22 12:52:36 +0600

    chore: implement the Task entity model

---
+++ server/models/Task.js
@@ -0,0 +1,58 @@
+// @ts-check
+
+import { Model } from 'objection';
+
+import Status from './Status.js';
+import User from './User.js';
+
+// @ts-ignore
+export default class Task extends Model {
+  static get tableName() {
+    return 'tasks';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name', 'statusId', 'creatorId'],
+      properties: {
+        id: { type: 'integer' },
+        statusId: { type: 'integer' },
+        creatorId: { type: 'integer' },
+        executorId: { type: 'integer' },
+        name: { type: 'string', minLength: 1 },
+        description: { type: 'string' },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      status: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Status,
+        join: {
+          from: 'tasks.statusId',
+          to: 'task_statuses.id',
+        },
+      },
+      creator: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: 'tasks.creatorId',
+          to: 'users.id',
+        },
+      },
+
+      executor: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: 'tasks.executorId',
+          to: 'users.id',
+        },
+      },
+    };
+  }
+}
