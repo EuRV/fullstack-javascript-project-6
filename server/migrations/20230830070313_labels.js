b76fc18fd1e3830d8afda9c0299889be47bc7ece
EuRV <eugenirovensky@yandex.ru>
2023-08-30 14:14:18 +0600

    chore: add a labels migration

---
+++ server/migrations/20230830070313_labels.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+export const up = (knex) => (
+  knex.schema.createTable('labels', (table) => {
+    table.increments('id').primary();
+    table.string('name');
+    table.timestamps(true, true);
+  })
+);
+
+export const down = (knex) => knex.schema.dropTable('labels');
