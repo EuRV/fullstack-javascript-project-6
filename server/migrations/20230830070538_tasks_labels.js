6b40450ce352e5e7fec8490d8a441f7778845db8
EuRV <eugenirovensky@yandex.ru>
2023-08-30 14:15:03 +0600

    chore: add a tasks_labels migration

---
+++ server/migrations/20230830070538_tasks_labels.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+export const up = (knex) => (
+  knex.schema.createTable('tasks_labels', (table) => {
+    table.increments('id').primary();
+    table.integer('task_id');
+    table.integer('label_id');
+    
+    table.foreign('task_id').references('id').inTable('tasks').onDelete('CASCADE');
+    table.foreign('label_id').references('id').inTable('labels').onDelete('RESTRICT');
+  })
+);
+
+export const down = (knex) => knex.schema.dropTable('tasks_labels');
