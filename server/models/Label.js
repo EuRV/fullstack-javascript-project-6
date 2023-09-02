509f92a5d1fbf8b8bfb60c10636fdc9beaf9f05c
EuRV <eugenirovensky@yandex.ru>
2023-09-02 18:13:02 +0600

    feat: implement the label model

---
+++ server/models/Label.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+import objectionUnique from 'objection-unique';
+import { Model } from 'objection';
+
+const unique = objectionUnique({ fields: ['name'] });
+
+export default class Label extends unique(Model) {
+  static get tableName() {
+    return 'labels';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name'],
+      properties: {
+        id: { type: 'integer' },
+        name: { type: 'string', minLength: 1 },
+      },
+    };
+  }
+}
