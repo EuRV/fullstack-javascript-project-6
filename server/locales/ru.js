af54a7d6d9292e5940a4d492f369452a56c22d7c
EuVR <eugenirovensky@yandex.ru>
2025-05-27 18:44:37 +0600

    feat: implement a localization dictionary

---
+++ server/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    appName: 'Менеджер задач',
+    layouts: {
+      application: {
+        users: 'Пользователи',
+        signIn: 'Вход',
+        signUp: 'Регистрация',
+        signOut: 'Выход',
+      },
+    },
+    welcome: {
+        index: {
+          hello: 'Привет от Хекслета!',
+          description: 'Практические курсы по программированию',
+          more: 'Узнать Больше',
+        },
+      },
+  }
+}
