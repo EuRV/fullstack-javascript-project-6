7cc23de548a91df36aa8d6b47fe164895b9ecb8a
EuVR <eugenirovensky@yandex.ru>
2025-06-02 13:02:50 +0600

    feat: implement the user model

---
+++ server/models/User.cjs
@@ -0,0 +1,24 @@
+const objectionUnique = require('objection-unique');
+const BaseModel = require('./BaseModel.cjs');
+
+const unique = objectionUnique({ fields: ['email'] });
+
+module.exports = class User extends unique(BaseModel) {
+  static get tableName() {
+    return 'users';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['email', 'password', 'firstName', 'lastName'],
+      properties: {
+        id: { type: 'integer' },
+        firstName: { type: 'string', minLength: 1 },
+        lastName: { type: 'string', minLength: 1 },
+        email: { type: 'string', minLength: 1 },
+        password: { type: 'string', minLength: 3 },
+      },
+    };
+  }
+};
