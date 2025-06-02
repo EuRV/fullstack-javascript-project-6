fc16c22421679313f7c8e278ba86f6a879417dc0
EuVR <eugenirovensky@yandex.ru>
2025-06-02 17:08:01 +0600

    feat: implement the new user page route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,13 @@
 export default (app) => {
+  const objectionModels = app.objection.models;
+
   app
     .get('/users', (req, reply) => {
-      reply.render('welcome/index');
+      const users = [];
+      reply.render('users/index', { users });
     })
     .get('/users/new', (req, reply) => {
-      reply.render('welcome/index');
+      const user = new objectionModels.user();
+      reply.render('users/new', { user });
     });
 };
