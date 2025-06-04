66ca40d0e8040afb2c5e1da61f6f3621c805e100
EuVR <eugenirovensky@yandex.ru>
2025-06-04 12:19:06 +0600

    feat: implement adding a new user to the database

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,5 +10,19 @@ export default (app) => {
     .get('/users/new', (req, reply) => {
       const user = new objectionModels.user();
       reply.render('users/new', { user });
+    })
+    .post('/users', async (req, reply) => {
+      const user = new objectionModels.user();
+      user.$set(req.body.data);
+      
+      try {
+        const validUser = await objectionModels.user.fromJson(req.body.data);
+        await objectionModels.user.query().insert(validUser);
+        reply.redirect('/');
+      } catch ({ data }) {
+        reply.render('users/new', { user, errors: data });
+      }
+
+      return reply;
     });
 };
