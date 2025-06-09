7a6d64074f6fadb537a525ff11a79f5521395010
EuVR <eugenirovensky@yandex.ru>
2025-06-09 17:47:16 +0600

    feat: implement the edit user route

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,18 +3,23 @@ export default (app) => {
 
   app
     .get('/users', async (req, reply) => {
-        const users = await objectionModels.user.query();
-        reply.render('users/index', { users });
-        return reply;
+      const users = await objectionModels.user.query();
+      reply.render('users/index', { users });
+      return reply;
     })
     .get('/users/new', (req, reply) => {
       const user = new objectionModels.user();
       reply.render('users/new', { user });
     })
+    .get('/users/:id/edit', async (req, reply) => {
+      const [user] = await objectionModels.user.query().where('id', req.params.id);
+      reply.render('users/edit', { user });
+      return reply;
+    })
     .post('/users', async (req, reply) => {
       const user = new objectionModels.user();
       user.$set(req.body.data);
-      
+
       try {
         const validUser = await objectionModels.user.fromJson(req.body.data);
         await objectionModels.user.query().insert(validUser);
