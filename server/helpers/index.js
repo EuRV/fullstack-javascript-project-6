399dcbf61852683d8afc081500c5725bbab9cfdc
EuVR <eugenirovensky@yandex.ru>
2025-06-10 22:49:32 +0600

    refactor: edit according to the operator-linebreak rule.

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -4,42 +4,44 @@ export default (app) => ({
   t(key) {
     return i18next.t(key);
   },
+
   formatDate(str) {
     const date = new Date(str);
     return date.toLocaleString();
   },
+
   isEmpty(value) {
-  if (value == null) {
-    return true;
-  }
-  
-  if (typeof value !== 'object' && typeof value !== 'function') {
-    return true;
-  }
+    if (value == null) {
+      return true;
+    }
 
-  if (Array.isArray(value) || typeof value === 'string' || 
-      (typeof value.length === 'number' && value.length > 0 && 
-       (value.length - 1) in value)) {
-    return value.length === 0;
-  }
+    if (typeof value !== 'object' && typeof value !== 'function') {
+      return true;
+    }
 
-  if (value instanceof Map || value instanceof Set) {
-    return value.size === 0;
-  }
-
-  if (typeof value === 'object') {
-    if (typeof Buffer !== 'undefined' && Buffer.isBuffer(value)) {
+    if (Array.isArray(value) || typeof value === 'string'
+      || (typeof value.length === 'number' && value.length > 0 && (value.length - 1) in value)) {
       return value.length === 0;
     }
 
-    const proto = Object.getPrototypeOf(value);
-    if (proto === null || proto === Object.prototype) {
-      return Object.keys(value).length === 0;
+    if (value instanceof Map || value instanceof Set) {
+      return value.size === 0;
     }
-  }
 
-  return false;
-},
+    if (typeof value === 'object') {
+      if (typeof Buffer !== 'undefined' && Buffer.isBuffer(value)) {
+        return value.length === 0;
+      }
+
+      const proto = Object.getPrototypeOf(value);
+      if (proto === null || proto === Object.prototype) {
+        return Object.keys(value).length === 0;
+      }
+    }
+
+    return false;
+  },
+
   get(obj, key, defaultValue = undefined) {
     const result = obj[key] || defaultValue;
     return result;
