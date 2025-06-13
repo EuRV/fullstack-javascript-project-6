58067cccde0318ce0418d191c45c991315d3b930
EuRV <eugenirovensky@yandex.ru>
2025-06-13 12:28:34 +0600

    refactor: delete session route

--- server/routes/welcome.js
+++ server/routes/welcome.js
@@ -2,8 +2,5 @@ export default (app) => {
   app
     .get('/', (req, reply) => {
       reply.render('welcome/index');
-    })
-    .get('/session/new', (req, reply) => {
-      reply.render('welcome/index');
     });
 };
