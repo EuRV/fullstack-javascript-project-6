3a0ed7d1a2379cd81e7f181112cd2957c2173738
EuVR <eugenirovensky@yandex.ru>
2025-06-16 14:36:52 +0600

    feat: implement signIn signOut route processing

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,7 +1,29 @@
 export default (app) => {
-	app
-		.get('/session/new', (req, reply) => {
-			const signInForm = {};
-			reply.render('session/new', { signInForm });
-		});
+  app
+    .get('/session/new', (request, reply) => {
+      const signInForm = {};
+      reply.render('session/new', { signInForm });
+    })
+    .post('/session', app.fp.authenticate('form', async (request, reply, err, user) => {
+      if (err) {
+        return app.httpErrors.internalServerError(err);
+      }
+      if (!user) {
+        const signInForm = request.body.data;
+        const errors = {
+          email: [{
+            message: 'Неверная почта или пароль',
+          }],
+        };
+        reply.render('session/new', { signInForm, errors });
+        return reply;
+      }
+      await request.logIn(user);
+      reply.redirect('/');
+      return reply;
+    }))
+    .delete('/session', (request, reply) => {
+      request.logOut();
+      reply.redirect('/');
+    });
 };
