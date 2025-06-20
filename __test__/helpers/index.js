e25e82246c8fc1517657f37cf3acb635af209349
EuVR <eugenirovensky@yandex.ru>
2025-06-20 19:32:09 +0600

    test: implement the helper functions

---
+++ __test__/helpers/index.js
@@ -0,0 +1,45 @@
+import { URL } from 'url';
+import fs from 'fs';
+import path from 'path';
+import { faker } from '@faker-js/faker';
+
+const getFixturePath = (filename) => path.join('..', '..', '__fixtures__', filename);
+const readFixture = (filename) => fs.readFileSync(new URL(getFixturePath(filename), import.meta.url), 'utf-8').trim();
+const getFixtureData = (filename) => JSON.parse(readFixture(filename));
+
+export const fakeUser = {
+  id: faker.string.uuid(),
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  email: faker.internet.email(),
+};
+
+export const getTestData = () => getFixtureData('testData.json');
+
+export const prepareUsersData = async (app) => {
+  const { knex } = app.objection;
+
+  await knex('users').insert(getFixtureData('users.json'));
+};
+
+export const signInUser = async (app) => {
+  prepareUsersData(app);
+  const responseSignIn = await app.inject({
+    method: 'POST',
+    url: '/session',
+    payload: {
+      data: getTestData().users.existing,
+    },
+  });
+
+  const [sessionCookie] = responseSignIn.cookies;
+  const { name, value } = sessionCookie;
+
+  return { [name]: value };
+};
+
+export const truncateTables = async (knex) => {
+  await Promise.all([
+    knex('users').truncate()
+  ]);
+};
