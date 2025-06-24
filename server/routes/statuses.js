371b335e7c779610a2ef085fed33c6f1f2f91f97
EuVR <eugenirovensky@yandex.ru>
2025-06-24 16:44:08 +0600

    feat: implement the creation of a new status
    Implement the display of the status creation page and adding a record to the database

--- server/routes/statuses.js
+++ server/routes/statuses.js
@@ -1,10 +1,32 @@
-// import i18next from 'i18next';
+import i18next from 'i18next';
 
 export default (app) => {
+  const objectionModels = app.objection.models;
+
   app
     .get('/statuses', { preValidation: app.authenticate }, async (request, reply) => {
-      const statuses = [];
+      const statuses = await objectionModels.status.query();
       reply.render('statuses/index', { statuses });
+      return reply;
+    })
+    .get('/statuses/new', { preValidation: app.authenticate }, async (request, reply) => {
+      const status = new objectionModels.status();
+      reply.render('statuses/new', { status });
+      return reply;
+    })
+    .post('/statuses', { preValidation: app.authenticate }, async (request, reply) => {
+      const status = new objectionModels.status();
+      status.$set(request.body.data);
+
+      try {
+        const validStatus = await objectionModels.status.fromJson(request.body.data);
+        await objectionModels.status.query().insert(validStatus);
+        request.flash('info', i18next.t('flash.statuses.create.success'));
+        reply.redirect('/statuses');
+      } catch ({ data }) {
+        request.flash('error', i18next.t('flash.statuses.create.error'));
+        reply.render('statuses/new', { status, errors: data });
+      }
       return reply;
     });
 };
