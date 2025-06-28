3b94495e2c425dd139db56cbe69f91f6cc2c6a74
EuRV <eugenirovensky@yandex.ru>
2025-06-28 15:26:18 +0600

    feat: implement the Task model

---
+++ server/models/Task.cjs
@@ -0,0 +1,49 @@
+const BaseModel = require('./BaseModel.cjs');
+
+module.exports = class Task extends BaseModel {
+  static get tableName() {
+    return 'tasks';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name', 'statusId', 'creatorId'],
+      properties: {
+        taskId: { type: 'integer' },
+        name: { type: 'string', minLength: 1 },
+        description: { type: 'string' },
+        statusId: { type: 'integer', minimum: 1 },
+        creatorId: { type: 'integer' },
+        executorId: { type: 'integer' },
+      },
+    };
+  }
+
+  static relationMappings = {
+    creator: {
+      relation: BaseModel.BelongsToOneRelation,
+      modelClass: 'User.cjs',
+      join: {
+        from: 'tasks.creatorId',
+        to: 'users.id',
+      },
+    },
+    executor: {
+      relation: BaseModel.BelongsToOneRelation,
+      modelClass: 'User.cjs',
+      join: {
+        from: 'tasks.executorId',
+        to: 'users.id',
+      },
+    },
+    status: {
+      relation: BaseModel.BelongsToOneRelation,
+      modelClass: 'Status.cjs',
+      join: {
+        from: 'tasks.statusId',
+        to: 'statuses.id',
+      },
+    },
+  };
+};
