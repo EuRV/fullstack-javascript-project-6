4dc1278df9bebbecc3e48859b898d32fac262207
EuRV <eugenirovensky@yandex.ru>
2025-06-28 15:20:53 +0600

    feat: implement the new task route

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,5 +4,12 @@ export default (app) => {
       const tasks = [];
       reply.render('tasks/index', { tasks });
       return reply;
+    })
+    .get('/tasks/new', { preValidation: app.authenticate }, async (request, reply) => {
+      const task = {};
+      const executors = [];
+      const statuses = [];
+      reply.render('tasks/new', { task, executors, statuses });
+      return reply;
     });
 };
