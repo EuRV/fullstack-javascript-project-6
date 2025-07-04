1f138b243c24cee7a7b122712d7acf4b331b5d25
EuVR <eugenirovensky@yandex.ru>
2025-07-04 21:22:21 +0600

    test: implement the remaining CRUD tests

--- __test__/statuses.test.js
+++ __test__/statuses.test.js
@@ -2,7 +2,7 @@ import { fastify } from 'fastify';
 
 import init from '../server/plugin.js';
 import {
-  getTestData, prepareUsersData, signInUser, truncateTables
+  getTestData, createRandomStatus, signInUser, truncateTables
 } from './helpers/index.js';
 
 describe('Status Routes CRUD operations', () => {
@@ -25,56 +25,44 @@ describe('Status Routes CRUD operations', () => {
     await knex.migrate.latest();
   });
 
-  // describe('POST /statuses - Create Status', () => {
-  //   it('should create a new status', async () => {
-  //     const statusData = {
-  //       name: faker.word.adjective(),
-  //     };
+  describe('POST /statuses - Create Status', () => {
+    it('should create a new status', async () => {
+      const params = createRandomStatus();
+      const authCookie = await signInUser(app);
+      const response = await app.inject({
+        method: 'POST',
+        url: '/statuses',
+        payload: {
+          data: params,
+        },
+        cookies: authCookie,
+      });
 
-  //     const response = await app.inject({
-  //       method: 'POST',
-  //       url: '/statuses',
-  //       payload: statusData,
-  //     });
+      expect(response.statusCode).toBe(302);
 
-  //     expect(response.statusCode).toBe(201);
-  //     const body = JSON.parse(response.body);
+      const status = await models.status.query().findOne({ name: params.name });
+      expect(status).toBeDefined();
+      expect(status).toMatchObject(params);
+    });
 
-  //     expect(body).toMatchObject({
-  //       name: statusData.name,
-  //     });
+    it('should throw validation error for invalid data', async () => {
+      const params = { name: '' };
+      const authCookie = await signInUser(app);
+      const response = await app.inject({
+        method: 'POST',
+        url: '/statuses',
+        payload: {
+          data: params,
+        },
+        cookies: authCookie,
+      });
 
-  //     // Проверяем в базе данных
-  //     const createdStatus = await Status.query().findById(body.id);
-  //     expect(createdStatus).toBeDefined();
-  //     expect(createdStatus?.name).toBe(statusData.name);
-  //   });
+      expect(response.statusCode).toBe(200);
 
-  //   it('should return error for duplicate name', async () => {
-  //     const name = faker.word.adjective();
-  //     await Status.query().insert({ name });
-
-  //     const response = await app.inject({
-  //       method: 'POST',
-  //       url: '/statuses',
-  //       payload: { name },
-  //     });
-
-  //     expect(response.statusCode).toBe(409);
-  //     expect(JSON.parse(response.body).message).toContain('already exists');
-  //   });
-
-  //   it('should return validation error for empty name', async () => {
-  //     const response = await app.inject({
-  //       method: 'POST',
-  //       url: '/statuses',
-  //       payload: { name: '' },
-  //     });
-
-  //     expect(response.statusCode).toBe(400);
-  //     expect(JSON.parse(response.body).message).toContain('validation failed');
-  //   });
-  // });
+      const statuses = await models.status.query();
+      expect(statuses).toEqual([]);
+    });
+  });
 
   describe('GET /statuses - List Statuses', () => {
     it('should return empty array when no statuses exist', async () => {
@@ -93,147 +81,106 @@ describe('Status Routes CRUD operations', () => {
       });
 
       expect(responseWithAuth.statusCode).toBe(200);
+
+      const statuses = await models.status.query();
+      expect(statuses).toEqual([]);
     });
 
-    // it('should return list of statuses', async () => {
-    //   const status1 = await Status.query().insert({
-    //     name: faker.word.adjective()
-    //   });
-    //   const status2 = await Status.query().insert({
-    //     name: faker.word.adjective()
-    //   });
+    it('should return list of statuses', async () => {
+      const status1 = await models.status.query().insert(createRandomStatus());
+      const status2 = await models.status.query().insert(createRandomStatus());
 
-    //   const response = await app.inject({
-    //     method: 'GET',
-    //     url: '/statuses',
-    //   });
+      const authCookie = await signInUser(app);
+      const response = await app.inject({
+        method: 'GET',
+        url: '/statuses',
+        cookies: authCookie,
+      });
 
-    //   expect(response.statusCode).toBe(200);
-    //   const statuses = JSON.parse(response.body);
+      expect(response.statusCode).toBe(200);
 
-    //   expect(statuses.length).toBe(2);
-    //   expect(statuses[0].name).toBe(status1.name);
-    //   expect(statuses[1].name).toBe(status2.name);
-    // });
+      const statuses = await models.status.query();
+
+      expect(statuses.length).toBe(2);
+      expect(statuses[0].name).toBe(status1.name);
+      expect(statuses[1].name).toBe(status2.name);
+    });
   });
 
-  // describe('GET /statuses/:id - Get Status by ID', () => {
-  //   it('should return status by ID', async () => {
-  //     const status = await Status.query().insert({
-  //       name: 'Test Status'
-  //     });
+  describe('PATCH /statuses/:id - Update Status', () => {
+    it('should return status by ID', async () => {
+      const status = await models.status.query().insert(createRandomStatus());
 
-  //     const response = await app.inject({
-  //       method: 'GET',
-  //       url: `/statuses/${status.id}`,
-  //     });
+      const authCookie = await signInUser(app);
+      const response = await app.inject({
+        method: 'GET',
+        url: `/statuses/${status.id}/edit`,
+        cookies: authCookie,
+      });
 
-  //     expect(response.statusCode).toBe(200);
-  //     const body = JSON.parse(response.body);
+      expect(response.statusCode).toBe(200);
+    });
 
-  //     expect(body).toMatchObject({
-  //       id: status.id,
-  //       name: 'Test Status',
-  //     });
-  //   });
+    it('should update status name', async () => {
+      const currentData = createRandomStatus();
+      const updatedData = createRandomStatus();
+      const { id } = await models.status.query().insert(currentData);
 
-  //   it('should return 404 for non-existent status', async () => {
-  //     const response = await app.inject({
-  //       method: 'GET',
-  //       url: '/statuses/9999',
-  //     });
+      const authCookie = await signInUser(app);
+      const response = await app.inject({
+        method: 'PATCH',
+        url: `/statuses/${id}`,
+        payload: {
+          data: updatedData,
+        },
+        cookies: authCookie,
+      });
 
-  //     expect(response.statusCode).toBe(404);
-  //     expect(JSON.parse(response.body).message).toContain('not found');
-  //   });
-  // });
+      expect(response.statusCode).toBe(302);
 
-  // describe('PUT /statuses/:id - Update Status', () => {
-  //   it('should update status name', async () => {
-  //     const status = await Status.query().insert({
-  //       name: 'Old Name'
-  //     });
+      const updatedStatus = await models.status.query().findById(id);
+      expect(updatedStatus.name).toBe(updatedData.name);
+    });
 
-  //     const updatedData = {
-  //       name: 'New Name',
-  //     };
+    it('should throw validation error for invalid data', async () => {
+      const currentData = createRandomStatus();
+      const updatedData = { name: '' };
+      const { id } = await models.status.query().insert(currentData);
 
-  //     const response = await app.inject({
-  //       method: 'PUT',
-  //       url: `/statuses/${status.id}`,
-  //       payload: updatedData,
-  //     });
+      const authCookie = await signInUser(app);
+      const response = await app.inject({
+        method: 'POST',
+        url: '/statuses',
+        payload: {
+          data: updatedData,
+        },
+        cookies: authCookie,
+      });
 
-  //     expect(response.statusCode).toBe(200);
-  //     const body = JSON.parse(response.body);
+      expect(response.statusCode).toBe(200);
 
-  //     expect(body).toMatchObject(updatedData);
+      const updatedStatus = await models.status.query().findById(id);
+      expect(updatedStatus.name).toBe(currentData.name);
+    });
+  });
 
-  //     // Проверяем в базе данных
-  //     const updatedStatus = await Status.query().findById(status.id);
-  //     expect(updatedStatus?.name).toBe('New Name');
-  //   });
+  describe('DELETE /statuses/:id - Delete Status', () => {
+    it('should delete status', async () => {
+      const status = await models.status.query().insert(createRandomStatus());
 
-  //   it('should return error for duplicate name', async () => {
-  //     const [status1, status2] = await Status.query().insert([
-  //       { name: 'Status 1' },
-  //       { name: 'Status 2' },
-  //     ]);
+      const authCookie = await signInUser(app);
+      const response = await app.inject({
+        method: 'DELETE',
+        url: `/statuses/${status.id}`,
+        cookies: authCookie,
+      });
 
-  //     const response = await app.inject({
-  //       method: 'PUT',
-  //       url: `/statuses/${status1.id}`,
-  //       payload: {
-  //         name: status2.name,
-  //       },
-  //     });
+      expect(response.statusCode).toBe(302);
 
-  //     expect(response.statusCode).toBe(409);
-  //     expect(JSON.parse(response.body).message).toContain('already exists');
-  //   });
-
-  //   it('should return 404 for non-existent status', async () => {
-  //     const response = await app.inject({
-  //       method: 'PUT',
-  //       url: '/statuses/9999',
-  //       payload: {
-  //         name: 'Updated',
-  //       },
-  //     });
-
-  //     expect(response.statusCode).toBe(404);
-  //     expect(JSON.parse(response.body).message).toContain('not found');
-  //   });
-  // });
-
-  // describe('DELETE /statuses/:id - Delete Status', () => {
-  //   it('should delete status', async () => {
-  //     const status = await Status.query().insert({
-  //       name: 'To Delete'
-  //     });
-
-  //     const response = await app.inject({
-  //       method: 'DELETE',
-  //       url: `/statuses/${status.id}`,
-  //     });
-
-  //     expect(response.statusCode).toBe(204);
-
-  //     // Проверяем удаление
-  //     const deletedStatus = await Status.query().findById(status.id);
-  //     expect(deletedStatus).toBeUndefined();
-  //   });
-
-  //   it('should return 404 for non-existent status', async () => {
-  //     const response = await app.inject({
-  //       method: 'DELETE',
-  //       url: '/statuses/9999',
-  //     });
-
-  //     expect(response.statusCode).toBe(404);
-  //     expect(JSON.parse(response.body).message).toContain('not found');
-  //   });
-  // });
+      const deletedStatus = await models.status.query().findById(status.id);
+      expect(deletedStatus).toBeUndefined();
+    });
+  });
 
   afterEach(async () => {
     await truncateTables(knex);
