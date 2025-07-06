a32b80cf77640515f79877face966a01cdd245cd
EuVR <eugenirovensky@yandex.ru>
2025-07-06 20:59:13 +0600

    feat: implement the creation of a new task

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,9 +1,13 @@
+import i18next from 'i18next';
+
 export default (app) => {
   const objectionModels = app.objection.models;
 
   app
     .get('/tasks', { preValidation: app.authenticate }, async (request, reply) => {
-      const tasks = [];
+      const tasks = await objectionModels.task
+          .query()
+          .withGraphJoined('[status, executor, creator]');
       reply.render('tasks/index', { tasks });
       return reply;
     })
@@ -15,6 +19,28 @@ export default (app) => {
       ]);
 
       reply.render('tasks/new', { task, executors, statuses });
+      return reply;
+    })
+    .post('/tasks', { preValidation: app.authenticate }, async (request, reply) => {
+      const task = new objectionModels.task();
+      const dataTask = {
+        ...request.body.data,
+        creatorId: request.session.get('passport').id,
+      }
+      task.$set(dataTask);
+
+      try {
+        await objectionModels.task.query().insert(dataTask);
+        request.flash('info', i18next.t('flash.tasks.create.success'));
+        reply.redirect('/tasks');
+      } catch ({ data }) {
+        request.flash('error', i18next.t('flash.tasks.create.error'));
+        const [executors, statuses] = await Promise.all([
+          objectionModels.user.query().modify('getFullName'),
+          objectionModels.status.query().modify('getShortData')
+        ]);
+        reply.render('tasks/new', { task, executors, statuses, errors: data });
+      }
       return reply;
     });
 };
