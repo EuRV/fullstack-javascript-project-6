a263c6d1e6cdcb59385975860edeb0ed5bd42ca9
EuVR <eugenirovensky@yandex.ru>
2025-07-09 22:43:24 +0600

    chore: add notnull constraint for required field

--- server/migrations/20250624094236_create_statuses_table.js
+++ server/migrations/20250624094236_create_statuses_table.js
@@ -5,7 +5,7 @@
 export const up = (knex) => (
   knex.schema.createTable('statuses', (table) => {
     table.increments('id').primary();
-    table.string('name');
+    table.string('name').notNullable().unique();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
