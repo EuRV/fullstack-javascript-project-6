16a176376296047ef5980fc33ba3046e25e48788
EuVR <eugenirovensky@yandex.ru>
2025-07-14 20:08:49 +0600

    test: implement the function create a random user

--- __test__/helpers/index.js
+++ __test__/helpers/index.js
@@ -2,17 +2,11 @@ import { URL } from 'url';
 import fs from 'fs';
 import path from 'path';
 import { faker } from '@faker-js/faker';
+import passport from '@fastify/passport';
 
 const getFixturePath = (filename) => path.join('..', '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(new URL(getFixturePath(filename), import.meta.url), 'utf-8').trim();
 const getFixtureData = (filename) => JSON.parse(readFixture(filename));
-
-export const fakeUser = {
-  id: faker.string.uuid(),
-  firstName: faker.person.firstName(),
-  lastName: faker.person.lastName(),
-  email: faker.internet.email(),
-};
 
 export const getTestData = () => getFixtureData('testData.json');
 
@@ -25,6 +19,24 @@ export const prepareUsersData = async (app) => {
 export const createRandomStatus = () => ({
   name: faker.word.adjective(),
 });
+
+export const createRandomUser = {
+  new() {
+    return {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+  },
+  update() {
+    return {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      password: faker.internet.password(),
+    }
+  }
+};
 
 export const signInUser = async (app) => {
   prepareUsersData(app);
