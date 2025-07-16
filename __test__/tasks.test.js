fbcbf8710a4aa6439062326e648c45264a1cb1bd
EuVR <eugenirovensky@yandex.ru>
2025-07-16 18:45:38 +0600

    test: implement tests for updating and deleting

--- __test__/tasks.test.js
+++ __test__/tasks.test.js
@@ -25,6 +25,18 @@ describe('Tasks Routes CRUD operations', () => {
   });
 
   describe('POST /tasks', () => {
+    it('should return form for new task', async () => {
+      const authCookie = await signInUser(app);
+      const response = await app.inject({
+        method: 'GET',
+        url: '/tasks/new',
+        cookies: authCookie,
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.payload).toMatch('Создание задачи');
+    });
+
     it('should create new task', async () => {
       const authCookie = await signInUser(app);
       const params = createRandomTask.new();
@@ -94,6 +106,123 @@ describe('Tasks Routes CRUD operations', () => {
 
       expect(response.statusCode).toBe(200);
       expect(response.payload).toMatch(name);
+    });
+
+    it('should return task details', async () => {
+      await prepareData(app);
+      const authCookie = await signInUser(app);
+      const { id, name } = await models.task.query().first();
+      const response = await app.inject({
+        method: 'GET',
+        url: `/tasks/${id}`,
+        cookies: authCookie,
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.payload).toMatch(name);
+    });
+  });
+
+  describe('PATCH /tasks/:id', () => {
+    it('should return edit form for task', async () => {
+      await prepareData(app);
+      const authCookie = await signInUser(app);
+      const { id } = await models.task.query().first();
+      const response = await app.inject({
+        method: 'GET',
+        url: `/tasks/${id}/edit`,
+        cookies: authCookie,
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.payload).toMatch('Изменение задачи');
+    });
+
+    it('should update task successfully', async () => {
+      await prepareData(app);
+      const authCookie = await signInUser(app);
+      const task = await models.task.query().first();
+      const params = createRandomTask.new();
+
+      const response = await app.inject({
+        method: 'PATCH',
+        url: `/tasks/${task.id}`,
+        payload: {
+          data: params,
+        }, 
+        cookies: authCookie,
+      });
+
+      expect(response.statusCode).toBe(302);
+      expect(response.headers.location).toBe('/tasks');
+
+      const updatedTask = await models.task.query().findById(task.id);
+      expect(updatedTask.name).toBe(params.name);
+      expect(updatedTask.name).not.toBe(task.name);
+    });
+
+    it('should throw validation error for invalid data', async () => {
+      await prepareData(app);
+      const authCookie = await signInUser(app);
+      const task = await models.task.query().first();
+      const params = createRandomTask.invalid();
+
+      const response = await app.inject({
+        method: 'PATCH',
+        url: `/tasks/${task.id}`,
+        payload: {
+          data: params,
+        },
+        cookies: authCookie,
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.payload).toMatch('Не удалось обновить задачу');
+
+      const updatedTask = await models.task.query().findById(task.id);
+      expect(updatedTask.name).not.toBe(params.name);
+      expect(updatedTask.name).toBe(task.name);
+    });
+  });
+
+  describe('DELETE /tasks/:id', () => {
+    it('should delete task successfully', async () => {
+      await prepareData(app);
+      const authCookie = await signInUser(app);
+      const task = await models.task.query().first();
+
+      const response = await app.inject({
+        method: 'DELETE',
+        url: `/tasks/${task.id}`,
+        cookies: authCookie,
+      });
+
+      expect(response.statusCode).toBe(302);
+      expect(response.headers.location).toBe('/tasks');
+
+      const deletedTask = await models.task.query().findById(task.id);
+      expect(deletedTask).toBeUndefined();
+    });
+
+    it('should not allow deletion if not owner', async () => {
+      await prepareData(app);
+      const authCookie = await signInUser(app);
+      const task = await models.task.query().insert({
+        ...createRandomTask.new(),
+        creatorId: 1,
+      });
+
+      const response = await app.inject({
+        method: 'DELETE',
+        url: `/tasks/${task.id}`,
+        cookies: authCookie,
+      });
+
+      expect(response.statusCode).toBe(302);
+      expect(response.headers.location).toBe('/tasks');
+
+      const stillExists = await models.task.query().findById(task.id);
+      expect(stillExists).toBeDefined();
     });
   });
 
