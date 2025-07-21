a1a5718f83c6dadc9ee620d3c11763a7b21f0ecd
EuVR <eugenirovensky@yandex.ru>
2025-07-21 15:50:05 +0600

    chore: change the database creation variable

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,7 @@ export const test = {
 
 export const production = {
   client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
+  connection: process.env.DATABASE_URL,
   // debug: true,
   useNullAsDefault: true,
   migrations,
