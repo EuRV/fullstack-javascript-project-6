ee2e43719f51b32f6b4c45ec4ff499344b71177b
EuVR <eugenirovensky@yandex.ru>
2025-07-22 16:05:11 +0600

    feat: add model label

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 import User from './User.cjs';
 import Status from './Status.cjs';
 import Task from './Task.cjs';
+import Label from './Label.cjs';
 
-export default [User, Status, Task];
+export default [User, Status, Task, Label];
