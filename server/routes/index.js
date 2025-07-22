b2f38a310391af211dcae87371a53674687cb31d
EuVR <eugenirovensky@yandex.ru>
2025-07-22 16:08:18 +0600

    feat: add routes labels

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,15 @@ import users from './users.js';
 import session from './session.js';
 import statuses from './statuses.js';
 import tasks from './tasks.js';
+import labels from './labels.js';
 
 const controllers = [
   welcome,
   users,
   session,
   statuses,
-  tasks
+  tasks,
+  labels
 ];
 
 export default (app) => controllers.forEach((f) => f(app));
