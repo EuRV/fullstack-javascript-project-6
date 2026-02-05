c0416c9ade33594196af45c1a26ace83baf702c3
EuVR <eugenirovensky@yandex.ru>
2026-02-05 20:09:14 +0600

    refactor(routes): define route paths as ROUTES constants

---
+++ server/routes/users/config.js
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  INDEX: '/users',
+  NEW: '/users/new',
+  EDIT: '/users/:id/edit',
+  CREATE: '/users',
+  UPDATE: '/users/:id',
+  DELETE: '/users/:id',
+  HOME: '/',
+};
