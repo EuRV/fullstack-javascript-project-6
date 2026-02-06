da427e8d6c6597024f70ccd071a70411e87b0d25
EuVR <eugenirovensky@yandex.ru>
2026-02-06 12:51:00 +0600

    refactor(routes): add VIEWS, FLASH_TYPES and FLASH_MESSAGES to config

--- server/routes/users/config.js
+++ server/routes/users/config.js
@@ -7,3 +7,25 @@ export const ROUTES = {
   DELETE: '/users/:id',
   HOME: '/',
 };
+
+export const VIEWS = {
+  INDEX: 'users/index',
+  NEW: 'users/new',
+  EDIT: 'users/edit',
+};
+
+export const FLASH_TYPES = {
+  SUCCESS: 'success',
+  ERROR: 'error',
+};
+
+export const FLASH_MESSAGES = {
+  USER_NOT_FOUND: 'flash.users.notFound',
+  CREATE_SUCCESS: 'flash.users.create.success',
+  CREATE_ERROR: 'flash.users.create.error',
+  UPDATE_SUCCESS: 'flash.users.update.success',
+  UPDATE_ERROR: 'flash.users.update.error',
+  DELETE_SUCCESS: 'flash.users.delete.success',
+  DELETE_ERROR: 'flash.users.delete.error',
+  COMMON_ERROR: 'flash.common.error',
+};
