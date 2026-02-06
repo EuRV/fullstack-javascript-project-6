a06482aa82f8ace3708d439cb55626cdee0e0847
EuVR <eugenirovensky@yandex.ru>
2026-02-06 12:42:18 +0600

    refactor(users): use VIEWS/ROUTES constants for views and redirects

--- server/routes/users/routes.js
+++ server/routes/users/routes.js
@@ -1,5 +1,5 @@
 import i18next from 'i18next';
-import { ROUTES } from './config.js';
+import { ROUTES, VIEWS } from './config.js';
 
 export default (app) => {
   const objectionModels = app.objection.models;
@@ -7,27 +7,27 @@ export default (app) => {
   app
     .get(ROUTES.INDEX, async (request, reply) => {
       const users = await objectionModels.user.query().modify('getPublicDate');
-      reply.render('users/index', { users });
+      reply.render(VIEWS.INDEX, { users });
       return reply;
     })
     .get(ROUTES.NEW, (request, reply) => {
       const user = new objectionModels.user();
-      reply.render('users/new', { user });
+      reply.render(VIEWS.NEW, { user });
     })
     .get(ROUTES.EDIT, { preValidation: app.authenticate, preHandler: app.requireCurrentUser }, async (request, reply) => {
       const { id } = request.params;
       const user = await objectionModels.user.query().findById(id);
-      reply.render('users/edit', { user });
+      reply.render(VIEWS.EDIT, { user });
       return reply;
     })
     .post(ROUTES.CREATE, async (request, reply) => {
       try {
         await objectionModels.user.query().insert(request.body.data);
         request.flash('info', i18next.t('flash.users.create.success'));
-        reply.redirect('/');
+        reply.redirect(ROUTES.HOME);
       } catch ({ data }) {
         request.flash('error', i18next.t('flash.users.create.error'));
-        reply.render('users/new', { user: request.body.data, errors: data });
+        reply.render(VIEWS.NEW, { user: request.body.data, errors: data });
       }
 
       return reply;
@@ -38,10 +38,10 @@ export default (app) => {
       try {
         await user.$query().patch(request.body.data);
         request.flash('info', i18next.t('flash.users.update.success'));
-        reply.redirect('/users');
+        reply.redirect(ROUTES.INDEX);
       } catch ({ data }) {
         request.flash('error', i18next.t('flash.users.update.error'));
-        reply.render('users/edit', { user, errors: data });
+        reply.render(VIEWS.EDIT, { user, errors: data });
       }
       return reply;
     })
@@ -53,11 +53,11 @@ export default (app) => {
         await user.$query().delete();
         request.logOut();
         request.flash('info', i18next.t('flash.users.delete.success'));
-        reply.redirect('/users');
+        reply.redirect(ROUTES.INDEX);
       } catch (error) {
         console.error(error);
         request.flash('error', i18next.t('flash.users.delete.error'));
-        reply.redirect('/users');
+        reply.redirect(ROUTES.INDEX);
       }
       return reply;
     });
