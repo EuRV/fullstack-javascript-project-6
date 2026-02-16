dfbbd4aa9c44295a63e5068d106887af4d528c43
EuVR <eugenirovensky@yandex.ru>
2026-02-16 15:48:49 +0600

    refactor(routes): use config constants for routes and messages

---
+++ server/routes/sessions/config.js
@@ -0,0 +1,24 @@
+export const ROUTES = {
+  NEW: '/session/new',
+  CREATE: '/session',
+  DELETE: '/session',
+  HOME: '/',
+};
+
+export const VIEWS = {
+  NEW: 'session/new',
+};
+
+export const FLASH_TYPES = {
+  INFO: 'info',
+  SUCCESS: 'success',
+  ERROR: 'error',
+};
+
+export const FLASH_MESSAGES = {
+  CREATE_SUCCESS: 'flash.session.create.success',
+  DELETE_SUCCESS: 'flash.session.delete.success',
+  // CREATE_ERROR: 'flash.session.create.error',
+  // DELETE_ERROR: 'flash.session.delete.error',
+  // COMMON_ERROR: 'flash.common.error',
+};
