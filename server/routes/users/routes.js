f75c0ff6563848ce7fca4a8fccb8d39e9fd5c531
EuVR <eugenirovensky@yandex.ru>
2026-02-16 12:30:25 +0600

    refactor(routes): delegate delete route to handler method

--- server/routes/users/routes.js
+++ server/routes/users/routes.js
@@ -1,6 +1,5 @@
-import i18next from 'i18next';
 import { UserHandler } from './handlers.js'
-import { ROUTES, VIEWS } from './config.js';
+import { ROUTES } from './config.js';
 
 export default (app) => {
   const handler = UserHandler(app);
@@ -18,20 +17,8 @@ export default (app) => {
       preValidation: app.authenticate,
       preHandler: app.requireCurrentUser,
     }, handler.updateUser)
-    .delete(ROUTES.DELETE, { preValidation: app.authenticate, preHandler: app.requireCurrentUser }, async (request, reply) => {
-      const { id } = request.params;
-      const user = await objectionModels.user.query().findById(id);
-
-      try {
-        await user.$query().delete();
-        request.logOut();
-        request.flash('info', i18next.t('flash.users.delete.success'));
-        reply.redirect(ROUTES.INDEX);
-      } catch (error) {
-        console.error(error);
-        request.flash('error', i18next.t('flash.users.delete.error'));
-        reply.redirect(ROUTES.INDEX);
-      }
-      return reply;
-    });
+    .delete(ROUTES.DELETE, {
+      preValidation: app.authenticate,
+      preHandler: app.requireCurrentUser,
+    }, handler.deleteUser);
 };
