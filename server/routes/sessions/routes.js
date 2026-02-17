f4db0e666ea18bddb52b01b58e288088b190c6ae
EuVR <eugenirovensky@yandex.ru>
2026-02-17 16:33:24 +0600

    efactor(routes): extract route handlers to dedicated module

--- server/routes/sessions/routes.js
+++ server/routes/sessions/routes.js
@@ -1,34 +1,11 @@
-import i18next from 'i18next';
-import { ROUTES, VIEWS, FLASH_TYPES, FLASH_MESSAGES } from './config.js';
+import { ROUTES  } from './config.js';
+import sessionHandlers from './handlers.js'
 
 export default (app) => {
+  const handlers = sessionHandlers(app);
+
   app
-    .get(ROUTES.NEW, (request, reply) => {
-      const signInForm = {};
-      reply.render(VIEWS.NEW, { signInForm });
-    })
-    .post(ROUTES.CREATE, app.fp.authenticate('form', async (request, reply, err, user) => {
-      if (err) {
-        return app.httpErrors.internalServerError(err);
-      }
-      if (!user) {
-        const signInForm = request.body.data;
-        const errors = {
-          email: [{
-            message: 'Неверная почта или пароль',
-          }],
-        };
-        reply.render(VIEWS.NEW, { signInForm, errors });
-        return reply;
-      }
-      await request.logIn(user);
-      request.flash(FLASH_TYPES.SUCCESS, i18next.t(FLASH_MESSAGES.CREATE_SUCCESS));
-      reply.redirect(ROUTES.HOME);
-      return reply;
-    }))
-    .delete(ROUTES.DELETE, (request, reply) => {
-      request.logOut();
-      request.flash(FLASH_TYPES.INFO, i18next.t(FLASH_MESSAGES.DELETE_SUCCESS));
-      reply.redirect(ROUTES.HOME);
-    });
+    .get(ROUTES.NEW, handlers.showLoginForm)
+    .post(ROUTES.CREATE, app.fp.authenticate('form', handlers.createSession))
+    .delete(ROUTES.DELETE, handlers.deleteSession);
 };
